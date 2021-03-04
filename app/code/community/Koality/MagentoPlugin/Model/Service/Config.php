c0871b02636a836347a1360193203eb9d1f8d775
Ole Schäfer <ole@customgento.com>
2021-03-04 14:21:02 +0100

    Improve configuration

--- app/code/community/Koality/MagentoPlugin/Model/Service/Config.php
+++ app/code/community/Koality/MagentoPlugin/Model/Service/Config.php
@@ -6,19 +6,19 @@ class Koality_MagentoPlugin_Model_Service_Config
 {
     public const KOALITY_API_KEY = 'koality_service/koality_apikey/apikey';
 
-    public const RUSHHOUR_BEGIN = 'koality_service/rush_hour/rushHourBegin';
+    public const RUSHHOUR_BEGIN = 'koality_service/orders_per_hour/rushHourBegin';
 
-    public const RUSHHOUR_END = 'koality_service/rush_hour/rushHourEnd';
+    public const RUSHHOUR_END = 'koality_service/orders_per_hour/rushHourEnd';
 
-    public const MIN_EXPECTED_ORDERS_PER_RUSHHOUR = 'koality_service/rush_hour/minOrdersPerHourRushHour';
+    public const MIN_EXPECTED_ORDERS_PER_RUSHHOUR = 'koality_service/orders_per_hour/minOrdersPerHourRushHour';
 
-    public const RUSHHOUR_INCLUDES_WEEKENDS = 'koality_service/rush_hour/includeWeekends';
+    public const RUSHHOUR_INCLUDES_WEEKENDS = 'koality_service/orders_per_hour/includeWeekends';
 
-    public const MIN_EXPECTED_ORDERS_PER_HOUR_NORMAL = 'koality_service/opening_hours/minOrdersPerHourNormal';
+    public const MIN_EXPECTED_ORDERS_PER_HOUR_NORMAL = 'koality_service/orders_per_hour/minOrdersPerHourNormal';
 
     public const MAX_EXPECTED_OPEN_CARTS = 'koality_service/open_carts/maxOpenCarts';
 
-    public const MIN_EXPECTED_ACTIVE_PRODUCTS = 'koality_service/active_products/activeProducts';
+    public const MIN_EXPECTED_ACTIVE_PRODUCTS = 'koality_service/active_products/minActiveProducts';
 
     public const MIN_EXPECTED_NEWSLETTER_SUBSCRIPTIONS = 'koality_service/newsletter_subscriptions/minNewsletterSubscriptions';
 
