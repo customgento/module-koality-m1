4655d809d816b74ee38bd8dbf0b978816790da4f
Ole Schäfer <ole@customgento.com>
2021-03-05 13:13:39 +0100

    Refactor

--- app/code/community/Koality/MagentoPlugin/sql/koality_magentoplugin_setup/install-1.0.0.php
+++ app/code/community/Koality/MagentoPlugin/sql/koality_magentoplugin_setup/install-1.0.0.php
@@ -1,19 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 /* @var Mage_Core_Model_Resource_Setup $installer */
 $installer = $this;
 $installer->startSetup();
-
 $setup = $installer->getConnection();
-
 $setup->insert(
     $this->getTable('core_config_data'),
     [
         'path'  => 'koality_service/koality_apikey/apikey',
-        'value' => sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', random_int(0, 65535), random_int(0, 65535),
-            random_int(0, 65535), random_int(16384, 20479), random_int(32768, 49151), random_int(0, 65535),
-            random_int(0, 65535), random_int(0, 65535))
+        'value' => Mage::helper('koality_magentoplugin')->createRandomApiKey()
     ]
 );
-
 $installer->endSetup();
