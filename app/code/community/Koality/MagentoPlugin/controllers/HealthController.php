d8fabf8d9817c4b77f42c876302c00ccf14e2d12
Ole Schäfer <ole@customgento.com>
2021-03-08 08:20:19 +0100

    remove Mage_Api2 depedency

--- app/code/community/Koality/MagentoPlugin/controllers/HealthController.php
+++ app/code/community/Koality/MagentoPlugin/controllers/HealthController.php
@@ -8,17 +8,15 @@ class Koality_MagentoPlugin_HealthController extends Mage_Core_Controller_Front_
     {
         $currentApiKey = $this->getRequest()->getParam('apikey');
         if ($currentApiKey === null) {
-            $this->getResponse()->setHttpResponseCode(Mage_Api2_Model_Server::HTTP_INTERNAL_ERROR);
+            $this->getResponse()->setHttpResponseCode(401);
 
             return $this->getResponse()->setBody('API key is missing. Please provide an API key and try again.');
         }
         if ($currentApiKey !== Mage::getModel('koality_magentoplugin/service_config')->getApiKey()) {
-            $this->getResponse()->setHttpResponseCode(Mage_Api2_Model_Server::HTTP_INTERNAL_ERROR);
+            $this->getResponse()->setHttpResponseCode(401);
 
             return $this->getResponse()->setBody('API key does not match. Please check API key and try again.');
         }
-        $this->getResponse()->setHttpResponseCode(Mage_Api2_Model_Server::HTTP_OK);
-
         return $this->getResponse()->setBody(Mage::getModel('koality_magentoplugin/checkCollector')
             ->generateResponse());
     }
