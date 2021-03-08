06a3fe200829dfca47c089604a20a05ace56b860
Ole Schäfer <ole@customgento.com>
2021-03-08 07:55:42 +0100

    Use constant for config path

--- app/code/community/Koality/MagentoPlugin/data/koality_magentoplugin_setup/data-install-1.0.0.php
+++ app/code/community/Koality/MagentoPlugin/data/koality_magentoplugin_setup/data-install-1.0.0.php
@@ -6,7 +6,7 @@ $setup     = $installer->getConnection();
 $setup->insert(
     $this->getTable('core_config_data'),
     [
-        'path'  => 'koality_service/koality_apikey/apikey',
+        'path'  => Koality_MagentoPlugin_Model_Service_Config::KOALITY_API_KEY,
         'value' => Mage::helper('koality_magentoplugin')->createRandomApiKey()
     ]
 );
