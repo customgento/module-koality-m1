cbc61bc840ae39efe11f49e6b50182115d6be7ce
Ole Schäfer <ole@customgento.com>
2021-03-08 08:34:53 +0100

    use snake case for config

--- app/code/community/Koality/MagentoPlugin/Model/Service/Config.php
+++ app/code/community/Koality/MagentoPlugin/Model/Service/Config.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 class Koality_MagentoPlugin_Model_Service_Config
 {
     public const KOALITY_API_KEY = 'koality_service/koality_apikey/apikey';
-    public const RUSHHOUR_BEGIN = 'koality_service/orders_per_hour/rushHourBegin';
-    public const RUSHHOUR_END = 'koality_service/orders_per_hour/rushHourEnd';
-    public const MIN_EXPECTED_ORDERS_PER_RUSHHOUR = 'koality_service/orders_per_hour/minOrdersPerHourRushHour';
-    public const RUSHHOUR_INCLUDES_WEEKENDS = 'koality_service/orders_per_hour/includeWeekends';
-    public const MIN_EXPECTED_ORDERS_PER_HOUR_NORMAL = 'koality_service/orders_per_hour/minOrdersPerHourNormal';
-    public const MAX_EXPECTED_OPEN_CARTS = 'koality_service/open_carts/maxOpenCarts';
-    public const MIN_EXPECTED_ACTIVE_PRODUCTS = 'koality_service/active_products/minActiveProducts';
+    public const RUSHHOUR_BEGIN = 'koality_service/orders_per_hour/rush_hour_begin';
+    public const RUSHHOUR_END = 'koality_service/orders_per_hour/rush_hour_end';
+    public const MIN_EXPECTED_ORDERS_PER_RUSHHOUR = 'koality_service/orders_per_hour/min_orders_per_hour_rush_hour';
+    public const RUSHHOUR_INCLUDES_WEEKENDS = 'koality_service/orders_per_hour/include_weekends';
+    public const MIN_EXPECTED_ORDERS_PER_HOUR_NORMAL = 'koality_service/orders_per_hour/min_orders_per_normal_hour';
+    public const MAX_EXPECTED_OPEN_CARTS = 'koality_service/open_carts/max_open_carts';
+    public const MIN_EXPECTED_ACTIVE_PRODUCTS = 'koality_service/active_products/min_active_products';
 
     public function getApiKey(): string
     {
@@ -20,12 +20,12 @@ class Koality_MagentoPlugin_Model_Service_Config
 
     public function getRushhourBegin(): string
     {
-        return Mage::getStoreConfig(self::RUSHHOUR_BEGIN);
+        return Mage::getStoreConfig(self::RUSHHOUR_BEGIN) ?: '';
     }
 
     public function getRushhourEnd(): string
     {
-        return Mage::getStoreConfig(self::RUSHHOUR_END);
+        return Mage::getStoreConfig(self::RUSHHOUR_END) ?: '';
     }
 
     public function getMinExpectedOrdersPerRushhour(): int
